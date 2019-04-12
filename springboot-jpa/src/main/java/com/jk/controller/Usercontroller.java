bd4fb132e0f1b5161c6ea3d04541d7a9c333f14b
“ht33 <13186266762@163.com>
2019-04-12 20:09:04 +0800

    有史以来第一次提交

---
+++ springboot-jpa/src/main/java/com/jk/controller/Usercontroller.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2019,金科
+ * FileName: Usercontroller
+ * Author:   HT
+ * Date:     2019/4/11 19:48
+ * History:
+ * HT          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈a〉
+ *
+ * @author Administrator
+ * @create 2019/4/11
+ * @since 1.0.0
+ */
+@RestController
+public class Usercontroller {
+
+
+}
+
---
+++ springboot-jpa/src/main/java/com/jk/model/BookBean.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2019,金科
+ * FileName: BookBean
+ * Author:   HT
+ * Date:     2019/4/11 20:06
+ * History:
+ * HT          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.model;
+
+import java.io.Serializable;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈a〉
+ *
+ * @author Administrator
+ * @create 2019/4/11
+ * @since 1.0.0
+ */
+public class BookBean implements Serializable {
+
+    private static final long serialVersionUID = 577452142924585610L;
+}
+
